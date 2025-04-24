c286989a3ee10a8104637a2ee2254bb5856f5cf5
elewo <elewo@ad.unc.edu>
2025-04-24 17:27:05 -0400

    Create DescriptionPlots.R
    some writing that may be useful

---
+++ DescriptionPlots.R
@@ -0,0 +1,15 @@
+##Catan is a strategic board game where players compete to build settlements, cities, and roads on a modular island map made up of hexagonal tiles that produce five key resources: lumber, brick (clay), wheat, sheep, and ore. These resources are essential for construction and progression, and players acquire them based on the roll of dice and the placement of their settlements at the start of the game.
+
+##Multiple factors play a role in a player’s success:
+  
+##Turn Position:
+##The order in which players place their starting settlements and take actions can affect their options and overall strategy.
+
+##Starting Resources:
+##The combination of resources a player has access to at the beginning of the game can affect what they are able to build and how they are able to proceed as the game progresses
+
+##Player turn Graph
+##This graph depicts the win rate based on the turn position in which each player starts at. With player 2 showing a much higher win rate than the others, moving to player 3, then 4, with player 1 having the lowest win rate overall. This would suggest it is best to begin the game in the turn position of 2 to have the best chance of winning, and to avoid the position of turn 1 with the lowest chance of leading to a win. 
+
+##Player Resource Graph
+##This graph depicts the win rate of each starting resource. These add up to over 100% as each player will start the game with multiple resources. Lumber, Ore, Sheep, and Wheat all have similar win rates with Sheep having the highest, while Clay has a much smaller win rate than the rest. This would suggest that Sheep is the most useful resource to start with and clay is the least useful, or least likely to lead to a win. 
